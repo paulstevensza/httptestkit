f90f84b66106cb27f7c5911a7d98b845e44f6e35
Paul Stevens <ps@xnode.co.za>
2018-01-22 14:17:21 +0200

    Additional plugins

---
+++ httptestkit/plugins/cert.py
@@ -0,0 +1,9 @@
+import requests
+
+from httptestkit.plugins import master
+
+
+class Cert(master.Plugin):
+
+    def lookup(self):
+        pass
---
+++ httptestkit/plugins/cloudflare.py
@@ -0,0 +1,9 @@
+import requests
+
+from httptestkit.plugins import master
+
+
+class CloudFlare(master.Plugin):
+
+    def lookup(self):
+        pass
